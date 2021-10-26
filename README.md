1f8fc8c5afe6940ab00772dad2d64dffd95fcc15
Patrick Vieira <pvieiradasilva420@gmail.com>
2021-10-26 19:24:42 -0300

    Add files
    Working ali project files and folders

---
+++ README.md
@@ -0,0 +1,23 @@
+##**Virtual Badge**
+
+_Status: Development_
+
+**###Description**
+_🖥️ Web project developed on the Origin track given by @maykbrito and @jakeliny2 at @Rocketseat's NLW Heat event_
+
+**### Technologies used**
++HTML
++CSS
++Javascipt
+
+##**Tools used**
++Visual Studio Code (VScode)
++Figma
+
+###**Links**
+{_Layout link on Figma_}(https://www.figma.com/file/QZu923g80I9vmKE3JAUTBe/%5BNLW-Heat---Mission%3A-Origin%5D-DoWhile2021-(Community)?node-id=0%3A1)
+
+_Page link:_
+
+###**Update to be done**
+_Landscap mode, tablet in Portrat mode and Landscap mode._
---
+++ mvc/view/javascript/main.js
@@ -0,0 +1,33 @@
+// Object social networks
+const LinksSocialMedia = {
+  github: 'patrick-engelmann',
+  youtube: 'channel/UC5deLrZBjZXMmdqoyVAOzOQ',
+  facebook: 'patrick.vieira.731572',
+  instagram: '_engelmann_97',
+  twitter: '_engelmann_97'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
